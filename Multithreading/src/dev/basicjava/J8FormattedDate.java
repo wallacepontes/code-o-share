35fd329b52f8dde0969c46b07bbaf51ad835999a
wallacepontes-admin <wallace.franklindepontes@readinessit.com>
2024-08-02 17:04:07 -0300

    Added the LocalDateTime example

--- Multithreading/src/dev/basicjava/J8FormattedDate.java
+++ Multithreading/src/dev/basicjava/J8FormattedDate.java
@@ -2,6 +2,7 @@ package dev.basicjava;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -10,16 +11,20 @@ public class J8FormattedDate {
 	public static void main(String[] args) {
 		// Get the current date
         LocalDate currentDate = LocalDate.now();
+        LocalDateTime currentDateTime = LocalDateTime.now();
 
         // Define the date format
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         // Format the current date
         String formattedDate = currentDate.format(formatter);
+        String formattedDateTime = currentDateTime.format(formatterTime);
 
         // Output the formatted date
         System.out.println(formattedDate);
-        
+     // Output the formatted date
+        System.out.println(formattedDateTime);
         //
         beforeJ8();
 
